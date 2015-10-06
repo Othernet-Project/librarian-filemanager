7450a1f3ebe3d0021aabc8adf203d3c1611ed54b
Andrean Franc <andrean.franc@gmail.com>
2015-10-06 16:22:07 +0200

    Revert "Fix partial inspection issue"
    This reverts commit 256b7a74bd75450c8c10d4f6c240e04ae9e99288.

--- librarian_filemanager/hooks.py
+++ librarian_filemanager/hooks.py
@@ -8,6 +8,5 @@ from .tasks import check_new_dirinfo
 def initialize(supervisor):
     supervisor.exts.openers = OpenerRegistry()
     supervisor.exts.menuitems.register(FilesMenuItem)
-    wrapped = partial(check_new_dirinfo, supervisor)
-    wrapped.__module__ = check_new_dirinfo.__module__
-    supervisor.events.subscribe('FILE_ADDED', wrapped)
+    supervisor.events.subscribe('FILE_ADDED', partial(check_new_dirinfo,
+                                                      supervisor))
