3bf7e56472218903a5f8c8c751c758ef663d5088
Andrean Franc <andrean.franc@gmail.com>
2015-11-12 18:18:29 +0100

    Adjust to new fs event api, and handle both dirinfo creation and deletion

--- librarian_filemanager/hooks.py
+++ librarian_filemanager/hooks.py
@@ -2,11 +2,11 @@ from functools import partial
 
 from .menuitems import FilesMenuItem
 from .openers import OpenerRegistry
-from .tasks import check_new_dirinfo
+from .tasks import check_dirinfo
 
 
 def initialize(supervisor):
     supervisor.exts.openers = OpenerRegistry()
     supervisor.exts.menuitems.register(FilesMenuItem)
-    supervisor.exts.events.subscribe('FILE_ADDED', partial(check_new_dirinfo,
-                                                           supervisor))
+    supervisor.exts.events.subscribe('FS_EVENT', partial(check_dirinfo,
+                                                         supervisor))
--- librarian_filemanager/tasks.py
+++ librarian_filemanager/tasks.py
@@ -1,6 +1,11 @@
+import os
+
 from .dirinfo import DirInfo
 
 
-def check_new_dirinfo(supervisor, fsobj):
-    if fsobj.name == DirInfo.FILENAME:
-        DirInfo.from_file(supervisor, fsobj.path)
+def check_dirinfo(supervisor, event):
+    if os.path.basename(event.src) == DirInfo.FILENAME:
+        if event.event_type == 'created':
+            DirInfo.from_file(supervisor, event.src)
+        elif event.event_type == 'deleted':
+            DirInfo.from_db(supervisor, event.src).delete()
