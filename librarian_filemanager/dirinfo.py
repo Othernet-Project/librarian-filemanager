bee7bb8465445512c9d7c57d19f4093bf867a7a6
Andrean Franc <andrean.franc@gmail.com>
2015-11-23 18:06:50 +0100

    Refactor dirinfo and move it's generic parts into the new CDFObject

--- librarian_filemanager/dirinfo.py
+++ librarian_filemanager/dirinfo.py
@@ -2,39 +2,53 @@ import logging
 import os
 import re
 
-from bottle_utils.common import to_bytes, to_unicode
+from bottle_utils.common import to_unicode
 
-from librarian_core.contrib.cache.utils import generate_key
+from librarian_content.library.base import CDFObject
 
 
-class DirInfo(object):
-    FILENAME = '.dirinfo'
+class DirInfo(CDFObject):
+    DATABASE_NAME = 'files'
+    TABLE_NAME = 'dirinfo'
     CACHE_KEY_TEMPLATE = u'dirinfo_{0}'
-    NO_LANGUAGE = ''
+    FILENAME = '.dirinfo'
     ENTRY_REGEX = re.compile(r'(\w+)\[(\w+)\]')
-
-    def __init__(self, supervisor, path, data=None):
-        self.supervisor = supervisor
-        self.path = path
-        self._info = data or dict()
+    NO_LANGUAGE = ''
 
     def get(self, language, key, default=None):
         try:
-            return self._info[language][key]
+            return self._data[language][key]
         except KeyError:
             try:
-                value = self._info[self.NO_LANGUAGE][key]
+                value = self._data[self.NO_LANGUAGE][key]
             except KeyError:
                 return default
             else:
                 return default if value is None else value
 
+    def store(self):
+        """Store dirinfo data structure in database."""
+        db = self.supervisor.exts.databases[self.DATABASE_NAME]
+        data = self._data or {self.NO_LANGUAGE: {}}
+        for language, info in data.items():
+            to_write = dict(path=self.path, language=language)
+            to_write.update(info)
+            query = db.Replace(self.TABLE_NAME, cols=to_write.keys())
+            db.query(query, **to_write)
+
+    def delete(self):
+        db = self.supervisor.exts.databases[self.DATABASE_NAME]
+        query = db.Delete(self.TABLE_NAME, where='path = ?')
+        db.query(query, self.path)
+        self.supervisor.exts.cache.delete(self.get_cache_key(self.path))
+
     def read_file(self):
         """Read dirinfo file from disk."""
         info_file_path = os.path.join(self.path, self.FILENAME)
-        if os.path.exists(info_file_path):
+        fsal = self.supervisor.exts.fsal
+        if fsal.exists(info_file_path):
             try:
-                with open(info_file_path, 'r') as info_file:
+                with fsal.open(info_file_path, 'r') as info_file:
                     info = info_file.readlines()
 
                 for line in info:
@@ -45,79 +59,19 @@ class DirInfo(object):
                     else:
                         language = self.NO_LANGUAGE
 
-                    self._info.setdefault(language, {})
-                    self._info[language][key] = to_unicode(value).strip()
+                    self._data.setdefault(language, {})
+                    self._data[language][key] = to_unicode(value).strip()
             except Exception:
-                self._info = dict()
+                self._data = dict()
                 msg = ".dirinfo reading of {0} failed.".format(self.path)
                 logging.exception(msg)
 
-    def read_db(self):
-        """Read dirinfo data from database."""
-        db = self.supervisor.exts.databases.files
-        query = db.Select(sets='dirinfo', where='path = ?')
-        db.query(query, self.path)
-        entries = db.results
-        if entries:
-            for entry in entries:
-                language = entry['language'] or self.NO_LANGUAGE
-                self._info[language] = dict((key, entry[key])
-                                            for key in entry.keys()
-                                            if key not in ('path', 'language'))
-            return True
-
-        return False
-
-    def save_to_db(self):
-        """Store dirinfo data structure in database."""
-        db = self.supervisor.exts.databases.files
-        info = self._info or {self.NO_LANGUAGE: {}}
-        for language, data in info.items():
-            to_write = dict(path=self.path, language=language)
-            to_write.update(data)
-            query = db.Replace('dirinfo', cols=to_write.keys())
-            db.query(query, **to_write)
-
-    def get_data(self):
-        return self._info
-
-    def delete(self):
-        db = self.supervisor.exts.databases.files
-        query = db.Delete('dirinfo', where='path = ?')
-        db.query(query, self.path)
-        self.supervisor.exts.cache.delete(self.get_cache_key(self.path))
-
     @classmethod
-    def get_cache_key(cls, path):
-        generated = generate_key(path)
-        return to_bytes(cls.CACHE_KEY_TEMPLATE.format(generated))
-
-    @classmethod
-    def from_file(cls, supervisor, path):
-        """Read dirinfo from disk, store it in database and put the retrieved
-        in the cache."""
-        dirinfo = cls(supervisor, path)
-        dirinfo.read_file()
-        dirinfo.save_to_db()
-        supervisor.exts.cache.set(cls.get_cache_key(path), dirinfo.get_data())
-        return dirinfo
-
-    @classmethod
-    def from_db(cls, supervisor, path):
-        """Read dirinfo from database and put the retrieved in the cache. If
-        no entries are found in the database, a background task will be
-        scheduled to read the file from disk."""
-        # attempt reading from cache first
-        key = cls.get_cache_key(path)
-        data = supervisor.exts(onfail=None).cache.get(key)
-        if data:
-            return cls(supervisor, path, data=data)
-        # if not in cache, get it from the database
-        dirinfo = cls(supervisor, path)
-        if dirinfo.read_db():
-            supervisor.exts.cache.set(key, dirinfo.get_data())
-        else:
-            supervisor.exts.tasks.schedule(DirInfo.from_file,
-                                           args=(supervisor, path))
-
-        return dirinfo
+    def fetch(cls, db, paths):
+        query = db.Select(sets=cls.TABLE_NAME, where=db.sqlin('path', paths))
+        db.query(query, *paths)
+        for row in db.results:
+            if row:
+                raw_data = cls.row_to_dict(row)
+                language = raw_data.pop('language', None) or cls.NO_LANGUAGE
+                yield (raw_data.pop('path'), {language: raw_data})
