9007959ba90cd22299200c31f5e4a752a9500051
Manish Mathai <manishpmathai@gmail.com>
2016-03-23 21:15:05 +0530

    Reload dirinfo when modified

--- librarian_filemanager/tasks.py
+++ librarian_filemanager/tasks.py
@@ -3,11 +3,27 @@ import os
 from .dirinfo import DirInfo
 
 
+def is_dirinfo(event):
+    if not event.is_dir:
+        name = os.path.basename(event.src)
+        return name == DirInfo.FILENAME
+    return False
+
+
+def delete_dirinfo(supervisor, path):
+    entries = DirInfo.from_db(supervisor, [path])
+    for dirinfo in entries.values():
+        dirinfo.delete()
+
+
 def check_dirinfo(supervisor, event):
-    if os.path.basename(event.src) == DirInfo.FILENAME:
-        if event.event_type == 'created':
-            DirInfo.from_file(supervisor, event.src)
-        elif event.event_type == 'deleted':
-            entries = DirInfo.from_db(supervisor, [event.src])
-            for dirinfo in entries.values():
-                dirinfo.delete()
+    if not is_dirinfo(event):
+        return
+    dirname = os.path.dirname(event.src)
+    if event.event_type == 'created':
+        DirInfo.from_file(supervisor, dirname)
+    elif event.event_type == 'modified':
+        delete_dirinfo(supervisor, dirname)
+        DirInfo.from_file(supervisor, dirname)
+    elif event.event_type == 'deleted':
+        delete_dirinfo(supervisor, dirname)
