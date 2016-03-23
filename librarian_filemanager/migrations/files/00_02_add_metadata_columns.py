e4012cce63c5bcfe9a4a09f6da144e186415b48b
Manish Mathai <manishpmathai@gmail.com>
2016-03-23 17:20:56 +0530

    Add metadata columns and default view column to dirinfo
    Metadata columns are: title, cover publisher, keywords

---
+++ librarian_filemanager/migrations/files/00_02_add_metadata_columns.py
@@ -0,0 +1,10 @@
+SQL = """
+alter table dirinfo add column title varchar;
+alter table dirinfo add column cover varchar;
+alter table dirinfo add column publisher varchar;
+alter table dirinfo add column keywords varchar;
+"""
+
+
+def up(db, conf):
+    db.executescript(SQL)
