add9395edb2d6e85bdda3d87b5b1f9b3e51fe74e
Andrean Franc <andrean.franc@gmail.com>
2016-03-23 18:50:07 +0100

    Use new list_descendants fsal endpoint for changelog

--- librarian_filemanager/routes.py
+++ librarian_filemanager/routes.py
@@ -116,7 +116,10 @@ def show_list_view(path, view, defaults):
         if view == 'html':
             data['index_file'] = find_html_index(paths)
         elif view == 'updates':
-            data['files'] = sorted(data['files'],
+            manager = Manager(request.app.supervisor)
+            span = request.app.config['changelog.span']
+            (_, _, files, _) = manager.list_descendants(path, span)
+            data['files'] = sorted(files,
                                    key=lambda x: x.create_date, reverse=True)
         elif view != 'generic':
             files = filter(lambda f: is_facet_valid(f.rel_path, view),
